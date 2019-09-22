31297df53d5e6fc7a0588a1e20f91a314fdcbb94
srz_zumix <zumix.cpp@gmail.com>
2019-09-22 14:49:16 +0900

    update

--- pm/auto-battle.air/auto-battle.py
+++ pm/auto-battle.air/auto-battle.py
@@ -50,33 +50,49 @@ def touch_result():
             pass
     return False
 
+def check_bar():
+    im = exists(Template(r"../../images/pm/bar2.png", record_pos=(-0.003, 0.935), resolution=(1080, 2160)))
+    if im:
+        pos = (im[0], im[1])
+        touch(pos)
+        pm_sleep(10)
+        return True
+    return False
+
+def is_wait_bar():
+    if check_bar():
+        if check_bar():
+            check_bar()
+        return True
+    return False
+
 def wait_battle():
     if not exists(Template(r"../../images/pm/result.png", record_pos=(0.049, 0.641), resolution=(1080, 2160))):
-        if exists(Template(r"../../images/pm/bar.png", record_pos=(-0.003, 0.935), resolution=(1080, 2160))):
-            pm_sleep(30)
-        else:
+        if not is_wait_bar():
             return
     if touch_result():
         while touch_result():
             pass
         while not touch_positive_button():
             pass
-        pm_sleep(3)
+        pm_sleep(6)
 
 def auto_battle(lv):
     while True:
         if touch_quest_banner(lv):
             touch_positive_button()
-            pm_sleep(5)
+            pm_sleep(10)
         else:
             touch_positive_button()
         wait_battle()
         # update()
 
 def main():
-    auto_battle(1)
+    auto_battle(2)
 
 main()
+
+
 
 
 
